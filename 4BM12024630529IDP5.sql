b4a1fd8355316536071858beea5963a3d5ca3473
DanielleSophia <96324106+DanielleSophia@users.noreply.github.com>
2025-06-17 09:38:31 -0600

    base de datos

---
+++ 4BM12024630529IDP5.sql
@@ -0,0 +1,11 @@
+DROP DATABASE login_db;
+CREATE DATABASE IF NOT EXISTS login_db;
+USE login_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL, 
+    type_user VARCHAR(50) NOT NULL);
+
+INSERT INTO users (username, password, type_user) VALUES ('admin', '1234', 'admin');
